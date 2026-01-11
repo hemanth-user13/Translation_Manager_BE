20d9178ac3e819d1061b6f53512f83abb9e0dc92
22210301016 <bolgamhemanth7878@gmail.com>
2026-01-11 12:49:16 +0530

    packages are added in the requitements txt file

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.api.routes import router
 from app.core.scheduler import start_scheduler
 
@@ -6,6 +7,14 @@ from app.core.scheduler import start_scheduler
 app=FastAPI(
     title="translation data exchange manager",
     version="1.0.0"
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # for personal tool; restrict in real prod
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 app.include_router(router)
